5bbe8bff79309a4e07f7182640922b743b0a93b1
Rinav-9 <rinavchaudhari9@gmail.com>
2025-07-25 23:26:19 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: window.location.hostname === 'localhost'
     ? 'http://localhost:5001/api' // ✅ hardcoded backend port
-    : 'https://excel-analytics-backend-sigma.onrender.com/api',
+    : 'https://excelanalytics-ciws.onrender.com',
   withCredentials: true,
 });
 
