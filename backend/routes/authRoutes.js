8ccc4d5f953c56d15f9d635d3e959413103fd0da
Rinav-9 <rinavchaudhari9@gmail.com>
2025-07-26 16:25:45 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const authController = require("../controllers/authController.js");
 const authMiddleware = require("../middleware/authMiddleware.js");
 
-router.post("api/auth/signup", authController.register);
-router.post("api/auth/login", authController.login);
-router.get("api/auth/profile", authMiddleware, authController.profile);
-router.put("api/auth/profile", authMiddleware, authController.updateProfile);
-router.post("api/auth/logout", authController.logout);
+router.post("/api/auth/signup", authController.register);
+router.post("/api/auth/login", authController.login);
+router.get("/api/auth/profile", authMiddleware, authController.profile);
+router.put("/api/auth/profile", authMiddleware, authController.updateProfile);
+router.post("/api/auth/logout", authController.logout);
 
-router.post("api/auth/forgot-password", authController.forgotPassword);
-router.post("api/auth/verify-otp", authController.verifyOtp);
-router.post("api/auth/reset-password", authController.resetPassword);
+router.post("/api/auth/forgot-password", authController.forgotPassword);
+router.post("/api/auth/verify-otp", authController.verifyOtp);
+router.post("/api/auth/reset-password", authController.resetPassword);
 
 module.exports = router;
